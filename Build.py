44db967af3fdd4a5612a50be82e5977bbb840f5d
itgk.j0x <itgk.j0x@gmail.com>
2025-05-27 12:02:38 +0900

    week8-hlf

--- Build.py
+++ Build.py
@@ -10,12 +10,10 @@ i = 0
 if not os.path.exists('Build/html'):
     os.makedirs('Build/html')
 
-while i <= (len(files)-1):
-    pathj = files[i]
+for pathj in files:
     html_exporter = HTMLExporter(template_name="classic")
     HTML = html_exporter.from_filename(pathj)
-    pathh = pathj.replace('.ipynb','.html')
+    pathh = pathj.replace('.ipynb', '.html')
     with open(pathh, 'w', encoding='utf-8') as f:
         f.write(HTML[0])
     shutil.move(pathh, './Build/html/')
-    i += 1 
