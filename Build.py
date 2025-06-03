4ea5a4a38ace214cfd1f2b6a7ae4ea94e131e371
itgk.j0x <itgk.j0x@gmail.com>
2025-06-03 10:26:44 +0900

    fix

--- Build.py
+++ Build.py
@@ -12,8 +12,8 @@ os.makedirs('Build/html')
 
 for pathj in files:
     html_exporter = HTMLExporter(template_name="classic")
-    HTML = html_exporter.from_filename(pathj)
+    body, resources = html_exporter.from_filename(pathj)
     pathh = pathj.replace('.ipynb', '.html')
     with open(pathh, 'w', encoding='utf-8') as f:
-        f.write(HTML[0])
+        f.write(body)
     shutil.move(pathh, './Build/html/')
