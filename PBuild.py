687129053b6c456fcdfe18a2d2e7ecb8d6c3c4bc
itgk.j0x <itgk.j0x@gmail.com>
2025-06-12 16:03:54 +0900

    use pdfkit

--- PBuild.py
+++ PBuild.py
@@ -3,6 +3,22 @@ import shutil
 import os
 from weasyprint import HTML
 from nbconvert import HTMLExporter
+import pdfkit
+
+# wkhtmltopdfの実行ファイルパスを設定
+# このパスは実際のインストールパスに合わせて変更してください
+config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
+options = {
+    'page-size': 'A4',
+    'margin-top': '0.1in',
+    'margin-right': '0.1in',
+    'margin-bottom': '0.1in',
+    'margin-left': '0.1in',
+    'encoding': "UTF-8",
+    'no-outline': None,
+    'disable-smart-shrinking': '',
+}
+
 
 
 files = glob.glob("Notebook/*/*.ipynb")
@@ -20,4 +36,8 @@ for pathj in files:
     html_exporter = HTMLExporter(template_name="classic")
     body, resources = html_exporter.from_filename(pathj)
     pathp = os.path.join('Build', 'pdf', os.path.basename(pathj).replace('.ipynb', '.pdf'))
-    HTML(string=body).write_pdf(pathp)
+    html_path = pathj.replace('.ipynb', '.html')
+    with open(html_path, 'w', encoding='utf-8') as f:
+        f.write(body)
+    pdfkit.from_file(html_path, pathp, options=options, configuration=config)
+    os.remove(html_path)
